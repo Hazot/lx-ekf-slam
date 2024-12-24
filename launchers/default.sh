67aa349951c936f50f10acc97bb6181ce6096339
Kevin Lessard <kevinlessard74@gmail.com>
2024-12-24 11:38:37 -0500

    first lx-recipe

--- launchers/default.sh
+++ launchers/default.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-python3 -m __solution__.main
+source /environment.sh
+
+source /opt/ros/noetic/setup.bash
+source /code/devel/setup.bash --extend
+
+exec roslaunch ekf_slam ekf_slam_node.launch veh:=$VEHICLE_NAME
